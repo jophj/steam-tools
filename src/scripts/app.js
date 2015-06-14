cdfc917218b17309728e843ad430618162a9a62a
Giovanni Pinto <jop.mezzana@gmail.com>
2015-06-14 18:01:34 +0200

    Refactoring: SteamAppProvider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,28 @@
 (function () {
-	var HOST = 'localhost:3666';
 	
-	var iSteamAppsApi = '/ISteamApps/GetAppList/v2/';
-	
-	var iSteamAppsRequest = new XMLHttpRequest();
-	var appDB = null;
-	
-	iSteamAppsRequest.onload = function (evt) {
-		appDB = JSON.parse(iSteamAppsRequest.responseText).applist.apps;
+	var SteamAppProvider = function () {
+			
+		var HOST = 'localhost:3666';
+		var iSteamAppsApi = '/ISteamApps/GetAppList/v2/';
+		var appDB = null;
+		return{
+			initDB: function (callback) {
+				var iSteamAppsRequest = new XMLHttpRequest();
+				
+				iSteamAppsRequest.onload = function (evt) {
+					appDB = JSON.parse(iSteamAppsRequest.responseText).applist.apps;
+					if(callback)
+					callback();
+				};
+				
+				iSteamAppsRequest.open("GET", 'http://' + HOST + iSteamAppsApi);
+				iSteamAppsRequest.send();
+			}
+		};
 	};
 	
-	iSteamAppsRequest.open("GET", 'http://' + HOST + iSteamAppsApi);
-	iSteamAppsRequest.send();
+	var dataprovider = SteamAppProvider();
+	dataprovider.initDB(null);
+	
+	
 })();
